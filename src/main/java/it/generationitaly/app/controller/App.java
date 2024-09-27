d2cff02a4216235d63ab71ab0455097b5b6f3aa0
flavi <flavi@LaptopDiFlavio>
2024-09-27 15:53:17 +0200

    add method 2

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -4,9 +4,14 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		metodo1();
+		metodo2();
 	}
 	
 	private static void metodo1() {
 		System.out.println("metodo 1");
+	}
+	
+	private static void metodo2() {
+		System.out.println("metodo 2");
 	}
 }
