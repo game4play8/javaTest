ad2daf93df15873a25fd82b666ab566f8d0710b4
flavi <flavi@LaptopDiFlavio>
2024-09-27 15:46:59 +0200

    added method 1

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -3,5 +3,10 @@ package it.generationitaly.app.controller;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		metodo1();
+	}
+	
+	private static void metodo1() {
+		System.out.println("metodo 1");
 	}
 }
